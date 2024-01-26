a7d139d37ea7d94da7a4e327a0f65b7c0c83c78c
Shahdad Daneshmand <115381298+Shahdad20@users.noreply.github.com>
2024-01-26 16:41:39 -0700

    property_value__per_community.py

--- Python Version/House Price Map/property_value__per_community.py
+++ Python Version/House Price Map/property_value__per_community.py
@@ -5,32 +5,42 @@ import plotly.express as px
 property_value_data = pd.read_csv(
     "Current_Year_Property_Assessments__Parcel__20231018.csv"
 )
-building_permits_data = pd.read_csv("Building_Permits_20231018.csv")
 
-property_value_data = property_value_data.rename(columns={"COMM_NAME": "CommunityName"})
+mean_property_value_by_community = (
+    property_value_data.groupby("COMM_NAME")["ASSESSED_VALUE"].mean().reset_index()
+)
 
-agg_data = property_value_data.groupby("CommunityName", as_index=False)[
-    "ASSESSED_VALUE"
-].mean()
-
-agg_data = agg_data[agg_data["ASSESSED_VALUE"] <= 30000000]
+mean_property_value_by_community = mean_property_value_by_community[
+    mean_property_value_by_community["ASSESSED_VALUE"] <= 10000000
+]
 
 community_boundaries = gpd.read_file("Community Boundaries 2011_20231018.geojson")
 
-merged_data = community_boundaries.merge(
-    agg_data, left_on="name", right_on="CommunityName", how="left"
+merged_data_all = community_boundaries.merge(
+    mean_property_value_by_community,
+    left_on="name",
+    right_on="COMM_NAME",
+    how="left",
 )
 
-fig = px.choropleth(
-    merged_data,
+map_center = {"lat": 51.0247, "lon": -114.0719}
+zoom_level = 9.8
+
+choropleth_map_all = px.choropleth_mapbox(
+    merged_data_all,
     geojson=community_boundaries.geometry,
-    locations=merged_data.index,
+    locations=merged_data_all.index,
     color="ASSESSED_VALUE",
-    color_continuous_scale=px.colors.sequential.Plasma,  
-    hover_name="CommunityName",
+    color_continuous_scale=px.colors.sequential.YlOrRd,
+    hover_name="COMM_NAME",
     title="Calgary Mean Property Value by Community",
+    labels={"ASSESSED_VALUE": "Mean Property Value"},
+    mapbox_style="carto-positron",
+    center=map_center,
+    zoom=zoom_level,
+    opacity=0.5,  
 )
 
-fig.update_geos(center_lon=-114.0719, center_lat=51.0447, projection_scale=450)
+choropleth_map_all.update_layout(title_font=dict(family="Times New Roman", size=28))
 
-fig.show()
+choropleth_map_all.show()

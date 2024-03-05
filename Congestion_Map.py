44641c0baba4fad8688d996364bb0db809f20a4d
Asma <asmahashmi94@gmail.com>
2024-03-04 21:26:20 -0700

    Fixing congestion and housing development maps

--- Congestion_Map.py
+++ Congestion_Map.py
@@ -3,8 +3,17 @@ import plotly.express as px
 import geopandas as gpd
 
 def get_data():
+
+    property_value_data = pd.read_csv("Current_Year_Property_Assessments__Parcel__20231018.csv")
+    
+    building_permits_data = pd.read_csv("Building_Permits_20231018.csv")
+    
     population_data = pd.read_csv("Census_by_Community_2017_20231020.csv")
+    
+    property_value_data = property_value_data.rename(columns={"COMM_NAME": "NAME"})
+    
     agg_data = population_data.groupby("NAME", as_index=False)["RES_CNT"].sum()
+   
     agg_data.rename(columns={"RES_CNT": "Population"}, inplace=True)
 
     community_boundaries = gpd.read_file("Community Boundaries 2011_20231018.geojson")
@@ -13,6 +22,7 @@ def get_data():
         agg_data, left_on="name", right_on="NAME", how="left"
     )
 
+    # Create choropleth map using Plotly Express
     fig = px.choropleth(
         merged_data,
         geojson=community_boundaries.geometry,
@@ -22,5 +32,9 @@ def get_data():
         hover_name="NAME",
         title="Calgary Population by Community",
     )
+    
     fig.update_geos(center_lon=-114.0719, center_lat=51.0447, projection_scale=450)
+    
     return fig
+
+get_data()

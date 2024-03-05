ebc3f4ef8f732b4f780e6e9e2aee2823f65fc4be
Asma <asmahashmi94@gmail.com>
2024-03-05 14:25:23 -0700

    Modifying for fastAPI implementation

--- server.py
+++ server.py
@@ -1,32 +1,40 @@
-from flask import Flask, jsonify
-from flask_cors import CORS 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import Congestion_Map
 import property_value__per_community
 import Housing_Development_Zone
 import vacancy_per_community
 
-app = Flask(__name__)
-CORS(app,origins="*")
+app = FastAPI()
 
-@app.route('/api/congestion_map')
-def get_congestion_map_data():
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/api/congestion_map')
+async def get_congestion_map_data():
     data = Congestion_Map.get_data()
-    return jsonify(data.to_json())
+    return data.to_json()
 
-@app.route('/api/house_price_map')
-def get_house_price_map_data():
+@app.get('/api/house_price_map')
+async def get_house_price_map_data():
     data = property_value__per_community.get_data()
-    return jsonify(data.to_json())
+    return data.to_json()
 
-@app.route('/api/housing_development_zone')
-def get_housing_development_zone_data():
+@app.get('/api/housing_development_zone')
+async def get_housing_development_zone_data():
     data = Housing_Development_Zone.get_data()
-    return jsonify(data.to_json())
+    return data.to_json()
 
-@app.route('/api/community_vacancy')
-def get_community_vacancy_data():
+@app.get('/api/community_vacancy')
+async def get_community_vacancy_data():
     data = vacancy_per_community.get_data()
-    return jsonify(data.to_json())
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return data.to_json()
